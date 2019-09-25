c6348ca58f9f9df30f75d3e10381c388976a333a
Joseph A <jandrade008@gmail.com>
2019-09-24 20:49:07 -0700

    added function to script.js

--- script.js
+++ script.js
@@ -7,10 +7,12 @@ var number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var special = ["!#$%&*+-:;<=>?@[]^_{|}~"];
 
 generate.addEventListener("click", function(){
-    var randomletter = Math.floor(Math.random(letter.length));
-    var randomnumber = Math.floor(Math.random(number.length));
-    var randomspecial = Math.floor(Math.random(special.length));
+    for (var i = 0; i < 8 || i < 128; i++){
+        var randomletter = Math.floor(Math.random(letter.length));
+        var randomnumber = Math.floor(Math.random(number.length));
+        var randomspecial = Math.floor(Math.random(special.length));
+    }
 
     var characters = randomletter + randomnumber + randomspecial;
-    
+    password.textContent = characters
 });
