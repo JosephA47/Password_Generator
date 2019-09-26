c88cf13c5bf4d65596704ab967fd3c6e168dc7a4
Joseph A <jandrade008@gmail.com>
2019-09-25 21:10:51 -0700

    added function to script.js

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
 var generate = document.getElementById("generate");
 var copy = document.getElementById("copy");
-
+var password = document.getElementById("password");
 
 var str = ["abcdefghijklmnopqrstuvwxyz!#$%&*+-:;<=>?@[]^_{|}~0123456789"];
-var newpassword = "";
-var plength =  Math.floor(Math.random() * (8 -128)+8);
+var newpassword = '';
+var plength =  Math.floor(Math.random() * (8-128)+8);
 
 function randomNumber() {
     for (var i = 0; i < plength; i++){
         var random = Math.floor(Math.random() * str.length);
-        newpassword += str.substring(random, random+1);
+        newpassword += str.substring(random);
         
-        var password = document.getElementById("password");
-        password.textContent = "Password: " + newpassword;
-        console.log(newpassword);
     }
+    password.textContent = "Password: " + newpassword;
+    //console.log(newpassword);
 }
 
 generate.addEventListener("click", function(event) {
     event.preventDefault();
-
+    
     randomNumber();
 })
