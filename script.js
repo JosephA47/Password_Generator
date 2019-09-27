753c2d6c2dfc54a992e623ac018b7e7b3c9b8438
Joseph A <jandrade008@gmail.com>
2019-09-26 17:51:37 -0700

    Fixed loop and add copy to clip-board function

--- script.js
+++ script.js
@@ -2,22 +2,38 @@ var generate = document.getElementById("generate");
 var copy = document.getElementById("copy");
 var password = document.getElementById("password");
 
-var str = ["abcdefghijklmnopqrstuvwxyz!#$%&*+-:;<=>?@[]^_{|}~0123456789"];
+var str = "abcdefghijklmnopqrstuvwxyz!#$%&*+-:;<=>?@[]^_{|}~0123456789";
 var newpassword = '';
-var plength =  Math.floor(Math.random() * (8-128)+8);
+var plength =  Math.floor(Math.random() * 128+8);
 
 function randomNumber() {
     for (var i = 0; i < plength; i++){
         var random = Math.floor(Math.random() * str.length);
-        newpassword += str.substring(random);
+        newpassword += str.substring(random, random+1);
         
     }
     password.textContent = "Password: " + newpassword;
-    //console.log(newpassword);
+    console.log(newpassword);
+}
+
+function copyPassword() {
+
+    password.select();
+
+    document.execCommand("copy");
+    console.log(password.value);
 }
 
 generate.addEventListener("click", function(event) {
-    event.preventDefault();
-    
+    event.preventDefault(); 
+
     randomNumber();
+})
+
+copy.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    copyPassword();
+
+    copy.textContent = "Copied";
 })
